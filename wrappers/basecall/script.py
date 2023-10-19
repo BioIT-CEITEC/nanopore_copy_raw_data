d613be56a841062b07852614e35ff43767c8e41f
Lucie Bulantová <LuckaBulantova@gmail.com>
2023-10-19 10:47:54 +0200

    better readability with f string and variables

--- wrappers/basecall/script.py
+++ wrappers/basecall/script.py
@@ -19,15 +19,24 @@ flowcell_suffix_map = {
         'prom':'_prom',
         'mk1c':'_mk1c'
 }
+
 config_version = config_version_map[snakemake.params.data_type]
-command = "./"+str(snakemake.params.guppy_version)+\
-        "/bin/guppy_basecaller"+\
+basecaller = "/usr/local/share/apps/ont-guppy-cpu/bin/guppy_basecaller"
+reads_folder = str(snakemake.wildcards.reads_folder)
+guppy_version = str(snakemake.params.guppy_version)
+data_type = str(snakemake.params.data_type)
+bps = str(snakemake.params.bps)
+quality = str(snakemake.params.quality)
+cfg = flowcell_suffix_map[snakemake.params.flowcell]
+
+# better readability? 
+command = f"{basecaller} --input_path all_reads/{reads_folder} --save_path \
+basecalling_output/{reads_folder} --config {guppy_version}/data/{data_type}_r{config_version}_{bps}bps_{quality}{cfg}.cfg"
+
+""" command = "/usr/local/share/apps/ont-guppy-cpu/bin/guppy_basecaller" + \
         " --input_path all_reads/"+ str(snakemake.wildcards.reads_folder)+\
         " --save_path basecalling_output/"+str(snakemake.wildcards.reads_folder)+\
         " --config " + str(snakemake.params.guppy_version) + "/data/" + str(snakemake.params.data_type)+\
-        "_r" + config_version + "_" + str(snakemake.params.bps) + "bps_" + str(snakemake.params.quality)+\ 
-        flowcell_suffix_map[snakemake.params.flowcell] +".cfg"
-
+        "_r" + config_version + "_" + str(snakemake.params.bps) + "bps_" + str(snakemake.params.quality) + flowcell_suffix_map[snakemake.params.flowcell] +".cfg"
+ """
 shell(command)
-
-
